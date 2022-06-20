0174fdc16d1e674c7db1dac3fe608d97edb0a279
MinjonH <97527244+MinjonH@users.noreply.github.com>
2022-06-20 20:49:36 +0200

    Style login form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import '../styles/login.css';
-
 
 function SignUp() {
   return (
-    <form className='login_form'>
-        <h1 className='login_header'>CREATE NEW APPLE ID</h1>
-        <hr />
-        <input type='text' id='email' placeholder='name@example.com' />
-        <input type='text' id='fName' placeholder='First Name'/>
-        <input type='text' id='lName' placeholder='Last Name' />
-        <input type='date' id='bDate' placeholder='Birthdate' />
-        <Button variation='danger'>SIGN UP</Button>
-        <hr />
-        <Button><a href='./Login.js'>LOGIN</a></Button>
-    </form>
+    <div className='body'>
+      <form className='login_form'>
+          <h1>CREATE NEW APPLE ID</h1>
+          <hr />
+          <input type='text' id='email' placeholder='name@example.com' />
+          <input type='text' id='fName' placeholder='First Name'/>
+          <input type='text' id='lName' placeholder='Last Name' />
+          <input type='date' id='bDate' placeholder='Birthdate' />
+          <button className='signUp_Btn'>SIGN UP</button>
+          <hr />
+          <a id='login_Btn' href='./Login.js'>Login</a>
+      </form>
+    </div>
   )
 }
 
