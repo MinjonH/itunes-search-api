9761e92ec233de203acd0180a49c6881336cdf5c
MinjonH <97527244+MinjonH@users.noreply.github.com>
2022-06-21 20:59:07 +0200

    backend

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import SignUp from './components/SignUp';
+// import axios from 'axios';
+import './styles/App.css';
+
+//Bootstrap Styling
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Container from 'react-bootstrap/Container';
+// import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+
+//Components
+// import SignUp from './components/SignUp';
+// import Search from './components/Search';
+// import UserList from './components/UserList';
+
+
 
 function Home() {
   return (
     <div>
-      <SignUp/>
     </div>
   )
 }
