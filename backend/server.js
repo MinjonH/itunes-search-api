0559f6709dc730476af8f59c968b4ad2e2041ff7
MinjonH <97527244+MinjonH@users.noreply.github.com>
2022-07-13 13:32:01 +0200

    Changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -123,14 +123,21 @@ app.use((err, req, res, next) => {
 });
 
 
-//Server static assests
+//DEPLOYMENT
+
+__dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-    });
-  }
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
   
   //Listening on PORT
 const PORT = process.env.PORT || 3001;
